6548e7c4273be4ffd129d9110a50760c0387c38e
eternal3ncore <eternal3ncore@gmail.com>
2026-08-30 16:24:40 -0500

    Add precompiled header

---
+++ src/PCH.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cctype>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <iomanip>
+#include <iterator>
+#include <memory>
+#include <optional>
+#include <ranges>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
+
+#include <spdlog/sinks/basic_file_sink.h>
+
+using namespace std::literals;
+namespace logger = SKSE::log;
